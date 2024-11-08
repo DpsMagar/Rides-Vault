f1bcc4d1fc1fd6b2b73c20f5fa0505530106bf87
DpsMagar <dipeshpaharai@gmail.com>
2024-11-08 12:37:12 +0545

    postgreSQL setup

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Helmet, Boot, Pants, Jacket, Glove
 
 # Register your models here.
+admin.site.register(Helmet)
+admin.site.register(Boot)
+admin.site.register(Pants)
+admin.site.register(Jacket)
+admin.site.register(Glove)
