2f7b4cd439b1256738dabcd71d7a1fcb8aa63425
DpsMagar <dipeshpaharai@gmail.com>
2024-11-09 15:24:43 +0545

    minor frontend and db changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,60 +1,73 @@
 from django.db import models
 
 class Helmet(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
     brand = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     safety_rating = models.CharField(max_length=50)
     stock_quantity = models.PositiveIntegerField()
-
+    image=models.ImageField(upload_to='helmets', null=True, blank=True)
     def __str__(self):
         return f"{self.brand} Helmet ({self.size})"
 
 
 class Boot(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
     brand = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     waterproof = models.BooleanField(default=False)
     stock_quantity = models.PositiveIntegerField()
+    image=models.ImageField(upload_to='boots', null=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.brand} Boot ({self.size})"
 
 
 class Pants(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
     brand = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     material = models.CharField(max_length=100)
     stock_quantity = models.PositiveIntegerField()
+    image=models.ImageField(upload_to='Pants', null=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.brand} Pants ({self.size})"
 
 
 class Jacket(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
     brand = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     insulation_type = models.CharField(max_length=50)
     stock_quantity = models.PositiveIntegerField()
+    image=models.ImageField(upload_to='jackets', null=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.brand} Jacket ({self.size})"
 
 
 class Glove(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
     brand = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     grip_type = models.CharField(max_length=50)
     stock_quantity = models.PositiveIntegerField()
+    image=models.ImageField(upload_to='gloves', null=True, blank=True)
+    
 
     def __str__(self):
         return f"{self.brand} Glove ({self.size})"
--- frontend/src/Components/Products/ProductList.jsx
+++ frontend/src/Components/Products/ProductList.jsx
@@ -3,94 +3,81 @@ import mtThunder from '../../Images/Helmets/MT Thunder 3.png'
 
 function ProductList() {
   return (
-    <div className=' min-h-screen bg-customColor w-screen   p-10 '>
-      <div className='text-5xl font-teko'>Helmets:</div>
-      <div className=' p-3 mt-1 h-full w-full bg-slate-500  flex flex-wrap justify-start gap-4 '>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+    <div className='min-h-screen bg-customColor max-w-screen p-10 overflow-hidden'>
+      <div className='text-7xl font-teko'>Helmets:</div>
+      <div className='p-3 mx-9 mt-1 h-full w-full flex flex-wrap justify-start gap-7'>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
+        <div className='size-60 flex flex-col justify-center'>
             <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
+                <img src={mtThunder} alt="" />
             </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
+            <span className='w-auto mx-auto'>MT Thunder 3</span>
         </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
-            <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
-            </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
-        </div>
-        <div className='h-56 w-56 bg-slate-600 flex flex-col justify-center '>
-            <div className='size-48 mx-auto'>
-                <img src={mtThunder} alt="" srcset="" />
-            </div>
-            <span className=' w-auto mx-auto'>MT Thunder 3</span>
-        </div>
-        
       </div>
     </div>
   )
