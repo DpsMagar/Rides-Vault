71a140b16ec23e98a1acdc29eb27561c39014d79
dipesh <dipeshpaharai@gmail.com>
2025-01-22 14:43:35 +0545

    getting started with spring boot replacing the existing django backend with spring boot

---
+++ backendSpringBoot/src/main/java/com/ridesVault/ridesVault/RidesVaultApplication.java
@@ -0,0 +1,13 @@
+package com.ridesVault.ridesVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidesVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidesVaultApplication.class, args);
+	}
+
+}
---
+++ backendSpringBoot/src/test/java/com/ridesVault/ridesVault/RidesVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridesVault.ridesVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidesVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
