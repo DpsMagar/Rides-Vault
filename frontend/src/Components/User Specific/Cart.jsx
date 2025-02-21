3d4eb55cfbc620a4a2edb67dc8eab16790f4ba81
dipesh <dipeshpaharai@gmail.com>
2025-02-21 22:34:59 +0545

    image issue done
    now will work upon the cart item deletion and order placing

--- frontend/src/Components/User Specific/Cart.jsx
+++ frontend/src/Components/User Specific/Cart.jsx
@@ -95,7 +95,7 @@ function Cart() {
                     <li key={item.id} className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
                       <div className="shrink-0 relative">
                         <span className="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full border bg-customColor text-sm font-medium text-white shadow sm:-top-2 sm:-right-2">{item.quantity}</span>
-                        <img className="h-24 w-24 max-w-full rounded-lg object-cover" src={`http://localhost:8080/Images/${data.itemType}/${data.image}`} alt="" />
+                        <img className="h-24 w-24 max-w-full rounded-lg object-cover" src={`http://localhost:8080/Images/${item.itemType}/${item.image}`} alt="Image Here" />
                       </div>
 
                       <div className="relative flex flex-1 flex-col justify-between">
